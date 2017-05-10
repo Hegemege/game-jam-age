a9bf78d57531d87b22e0f02e48aab18ec3222b8e
Henri Niva <hegemege@gmail.com>
2017-05-10 10:58:57 +0300

    Code refactoring

---
+++ Assets/Scripts/ClimateController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Season
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter
+}
+
+public class ClimateController : MonoBehaviour 
+{
+    [HideInInspector]
+    public int Year;
+
+    // Season changing
+    private float seasonTimer;
+    public float SeasonLength;
+
+    public float TemperatureSpring;
+    public float TemperatureSummer;
+    public float TemperatureAutumn;
+    public float TemperatureWinter;
+
+    public float RainSpring;
+    public float RainSummer;
+    public float RainAutumn;
+    public float RainWinter;
+
+    public float SunlightSpring;
+    public float SunlightSummer;
+    public float SunlightAutumn;
+    public float SunlightWinter;
+
+    // Season parameters
+    [HideInInspector]
+    public float Rain;
+    [HideInInspector]
+    public float Sunlight;
+    [HideInInspector]
+    public float Temperature;
+
+    public Season StartingSeason;
+    [HideInInspector]
+    public Season CurrentSeason;
+
+    void Awake()
+    {
+        Year = 1;
+    }
+
+    void Start() 
+    {
+        
+    }
+    
+    void Update() 
+    {
+        
+    }
+
+
+    private void CheckSeason()
+    {
+        // Changes the season and the temperature after x seconds
+        seasonTimer += Time.deltaTime;
+        if (seasonTimer > SeasonLength)
+        {
+            seasonTimer = 0;
+            switch (CurrentSeason)
+            {
+                case Season.Spring:
+                    CurrentSeason = Season.Summer;
+                    break;
+                case Season.Summer:
+                    CurrentSeason = Season.Autumn;
+                    break;
+                case Season.Autumn:
+                    CurrentSeason = Season.Winter;
+                    break;
+                case Season.Winter:
+                    CurrentSeason = Season.Spring;
+                    Year += 1;
+                    break;
+            }
+        }
+
+    }
+
+    private void UpdateParameters()
+    {
+        switch (CurrentSeason)
+        {
+            case Season.Spring:
+                Temperature = TemperatureSpring;
+                Rain = RainSpring;
+                Sunlight = SunlightSpring;
+                break;
+            case Season.Summer:
+                Temperature = TemperatureSummer;
+                Rain = RainSummer;
+                Sunlight = SunlightSummer;
+                break;
+            case Season.Autumn:
+                Temperature = TemperatureAutumn;
+                Rain = RainAutumn;
+                Sunlight = SunlightAutumn;
+                break;
+            case Season.Winter:
+                Temperature = TemperatureWinter;
+                Rain = RainWinter;
+                Sunlight = SunlightWinter;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/TreeScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour
+{
+    private ClimateController climate;
+
+    public float GrowthRate;
+
+    public float InitialEnergy;
+    public float InitialWater;
+
+    [HideInInspector]
+    public float Energy; // Tree needs energy to grow (gain from photosynthesis)
+    [HideInInspector]
+    public float Water; // Tree needs water to survive (gain from rain + passively from ground)
+
+    public float MaxEnergy; // How much energy can be held in storage
+    public float MaxWater; // How much water can be held in storage
+
+    public int InitialLeaves;
+    [HideInInspector]
+    public int Leaves;
+
+    void Awake()
+    {
+        climate = GameObject.Find("ClimateController").GetComponent<ClimateController>();
+        Leaves = InitialLeaves;
+    }
+    
+	void Start ()
+    {
+
+    }
+	
+	void Update ()
+    {
+        Photosynthesis();
+        HandleWater();
+        Grow();
+
+        // Death stuff or whatever
+        if (Water >= MaxWater)
+        {
+            Debug.Log("The tree is rotten!");
+        }
+        else if (Water <= 0)
+        {
+            Debug.Log("The tree dried out!");
+        }
+
+
+    }
+
+    void Photosynthesis()
+    {
+        // Generates energy from sunlight and leaves
+        Energy += Leaves * climate.Sunlight * Time.deltaTime;
+
+        //energy = Mathf.Min(max_energy, energy);
+    }
+
+    void HandleWater()
+    {
+        // Collect water from rain
+        Water += climate.Rain * Time.deltaTime;
+
+        // Evaporate water because of the temperature (And sunlight?)
+        Water -= Mathf.Max(0, climate.Temperature * (climate.Sunlight + 1) * Time.deltaTime * 0.1f);
+    }
+
+    void Grow()
+    {
+        // Growth is affected by the current temperature and it needs water/energy
+        float growth = Time.deltaTime * climate.Temperature * GrowthRate;
+        growth = Mathf.Min(growth, Energy, Water);
+        growth = Mathf.Max(0, growth);
+        if (growth > 0)
+        {
+            // Growing consumes energy and water
+            Energy -= Time.deltaTime * climate.Temperature;
+            Water -= Time.deltaTime * climate.Temperature;
+        }
+    }
+
+}
