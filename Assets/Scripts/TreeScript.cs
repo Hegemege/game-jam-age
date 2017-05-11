2ac0923c83fe29fbf8ace247fb033a34c39a92ce
Anton Debner <anton.debner@aalto.fi>
2017-05-11 14:55:10 +0300

    Changes to water/energy use/gain

--- Assets/Scripts/TreeScript.cs
+++ Assets/Scripts/TreeScript.cs
@@ -10,6 +10,25 @@ public class TreeScript : MonoBehaviour
     private Text energyText;
     private Text waterText;
 
+    public float EnergyPerSecond;
+    public float WaterPerSecond;
+    public float WaterPerTemperature;
+    //public float WaterPerSunlight;
+
+    public float WaterGain;
+    public float EnergyGain;
+    public float WinterWaterGain;
+    public float WinterEnergyGain;
+    public Vector2 SpringWaterTarget;
+    public Vector2 SummerWaterTarget;
+    public Vector2 AutumnWaterTarget;
+    public Vector2 WinterWaterTarget;
+
+    public Vector2 SpringEnergyTarget;
+    public Vector2 SummerEnergyTarget;
+    public Vector2 AutumnEnergyTarget;
+    public Vector2 WinterEnergyTarget;
+
     private float size;
     private float growthRate; // Current growth rate
 
@@ -51,9 +70,11 @@ public class TreeScript : MonoBehaviour
         energyText.text = "Energy: " + Energy.ToString("F1");
         waterText.text = "Water: " + Water.ToString("F1");
 
-        Photosynthesis();
-        HandleWater();
-        Grow();
+
+        var season = climate.GetSeason();
+        CollectResources(season);
+        UseResources(season);
+        Grow(season);
 
         // Check status (Dead/Alive)
         if (Water >= MaxWater)
@@ -66,26 +87,46 @@ public class TreeScript : MonoBehaviour
         }
     }
 
-    private void Photosynthesis()
+    private void CollectResources(Season season)
     {
+        float energyGain = EnergyGain;
+        float waterGain = WaterGain;
+        if (season == Season.Winter)
+        {
+            energyGain = WinterEnergyGain;
+            waterGain = WinterWaterGain;
+        }
+
+        // Collect water from rain
+        Water += climate.GetRain() * Time.deltaTime * waterGain;
+        Water = Mathf.Clamp(Water, 0, MaxWater);
+
         // Generates energy from sunlight and leaves
-        Energy += Leaves * climate.GetSunlight() * Time.deltaTime;
+        Energy += Leaves * climate.GetSunlight() * Time.deltaTime * energyGain;
         Energy = Mathf.Clamp(Energy, 0, MaxEnergy);
     }
 
-    private void HandleWater()
+    private void UseResources(Season season)
     {
-        // Collect water from rain
-        Water += climate.GetRain() * Time.deltaTime;
+        // Staying alive requires water and energy
+        Energy -= EnergyPerSecond * Time.deltaTime;
+        Energy = Mathf.Clamp(Energy, 0, MaxEnergy);
 
-        // Evaporate water because of the temperature (And sunlight?)
-        Water -= Mathf.Max(0, climate.GetTemperature() * (climate.GetSunlight() + 1) * Time.deltaTime * 0.1f);
+        // Temperature (and sunlight) also evaporates water
+        Water -= WaterPerSecond * Time.deltaTime;
+        if (climate.GetTemperature() > 0)
+        {
+            Water -= WaterPerTemperature * climate.GetTemperature() * Time.deltaTime;
+        }
 
+        //Water -= climate.GetSunlight() * WaterPerSunlight * Time.deltaTime;
         Water = Mathf.Clamp(Water, 0, MaxWater);
     }
 
-    private void Grow()
+    private void Grow(Season season)
     {
+        var seasonParams = GetSeasonParamenters(season);
+
         // Growth is affected by the current temperature
         growthRate = climate.GetTemperature() * GrowthMultiplier;
 
@@ -105,4 +146,24 @@ public class TreeScript : MonoBehaviour
     {
         // TODO
     }
+
+    private Vector2[] GetSeasonParamenters(Season season)
+    {
+        switch (season)
+        {
+            case Season.Spring:
+                return new Vector2[] { SpringWaterTarget, SpringEnergyTarget};
+            case Season.Summer:
+                return new Vector2[] { SummerWaterTarget, SummerEnergyTarget };
+            case Season.Autumn:
+                return new Vector2[] { AutumnWaterTarget, AutumnEnergyTarget };
+            case Season.Winter:
+                return new Vector2[] { WinterWaterTarget, WinterEnergyTarget };
+            default:
+                Debug.LogError("Unknown season!" + season);
+                return new Vector2[] { SummerWaterTarget, SummerEnergyTarget };
+        }
+    }
+
+
 }
