2786b33dc44b1673e49be0489c820194db879adf
Anton Debner <anton.debner@aalto.fi>
2017-05-12 11:38:25 +0300

    Way too loud rain

--- Assets/Scripts/WeatherController.cs
+++ Assets/Scripts/WeatherController.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class WeatherController : MonoBehaviour
 {
+    private AudioSource audioSource;
     public ParticleSystem Rain;
     public ParticleSystem Snow;
+    public AudioClip RainSoundLow;
+    public AudioClip RainSoundHard;
+    public float RainVolume;
 
     private ClimateController climate;
 
@@ -14,11 +18,23 @@ public class WeatherController : MonoBehaviour
 
     private bool raining;
     private Season startedSeason;
+    private ParticleSystem.EmissionModule rainEmission;
+    private ParticleSystem.EmissionModule snowEmission;
 
     void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
+        if (!audioSource)
+        {
+            gameObject.AddComponent<AudioSource>();
+            audioSource = GetComponent<AudioSource>();
+
+        }
         climate = GameObject.Find("ClimateController").GetComponent<ClimateController>();
         weatherTime = RainCycle;
+
+        rainEmission = Rain.emission;
+        snowEmission = Snow.emission;
     }
 
     void Start() 
@@ -41,14 +57,14 @@ public class WeatherController : MonoBehaviour
         {
             if (climate.GetSeason() == Season.Winter && raining)
             {
-                Rain.Stop();
+                RainStop();
                 Snow.Play();
             }
 
             if (climate.GetSeason() == Season.Spring && raining)
             {
                 Snow.Stop();
-                Rain.Play();
+                RainPlay();
             }
 
             if (climate.GetSeason() == Season.Autumn)
@@ -86,19 +102,48 @@ public class WeatherController : MonoBehaviour
 
         if (!raining)
         {
-            Rain.Stop();
-            Snow.Stop();
+            RainStop();
+            SnowStop();
         }
         else
         {
             if (currentSeason == Season.Winter)
             {
-                Snow.Play();
+                SnowPlay();
             }
             else
             {
-                Rain.Play();
+                RainPlay();
             }
         }
+    }
+
+    private void RainPlay()
+    {
+        var rain = climate.GetRain();
+        if (rain > 0.5f)
+        {
+            audioSource.PlayOneShot(RainSoundHard);
+        }
+        else
+        {
+            audioSource.PlayOneShot(RainSoundLow);
+        }
+        
+        Rain.Play();
+    }
+    private void RainStop()
+    {
+        audioSource.Stop();
+        Rain.Stop();
+    }
+
+    private void SnowPlay()
+    {
+        Snow.Play();
+    }
+    private void SnowStop()
+    {
+        Snow.Stop();
     }
 }
